f1b9bea145afe5c49833d79a01d7308ba777a027
Ako Samuel <167370750+sasbaws221@users.noreply.github.com>
2024-08-18 22:50:19 -0700

    Update appointment.actions.ts

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,18 +1,14 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-
 import { ID, Query } from "node-appwrite";
-
 import { Appointment } from "@/types/appwrite.types";
-
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
   messaging,
 } from "../appwrite.config";
-
 import { parseStringify } from "../utils";
 
 //  CREATE APPOINTMENT
